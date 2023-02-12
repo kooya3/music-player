ec8d40effb37c6b34571a66420eb0a67e80f385d
kooya3 <elyeestatua@gmail.com>
2023-02-13 00:33:34 +0300

    debugging project files

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -7,7 +7,12 @@ import { playPause, setActiveSong } from '../redux/features/playerSlice';
 
 const SongCard = ({ song, isPlaying, activeSong, data, i }) => (
   <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
-  SongCard</div>
+    <div className="relative w-full h-56 group">
+    <div className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${activeSong?.title === song.title ? 'flex bg-black bg-opacity-70' : 'hidden'}`}>
+
+  </div>
+  </div>
+  </div>
 );
 
 export default SongCard;

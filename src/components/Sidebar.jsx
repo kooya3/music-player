ba28a815e2dcf69c819077d0ac41f908f53d4746
kooya3 <elyeestatua@gmail.com>
2023-02-13 02:22:10 +0300

    creating a mobile media query and sidebar section

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,22 @@
-const Sidebar = () => (
-  <div>Sidebar</div>
-);
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { HiOutlineHashtag, HiOutlineHome, HiOutlineMenu, HiOutlinePhotograph, HiOutlineUserGroup } from 'react-icons/hi';
+import { RiCloseLine } from 'react-icons/ri';
+
+import { logo } from '../assets';
+import { links } from '../assets/constants';
+
+
+
+const Sidebar = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  return (
+    <div className='md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]'>
+      <img src={logo} alt="logo" className='
+      w-full h-14 object-contain' />
+
+    </div>
+  )
+};
 
 export default Sidebar;

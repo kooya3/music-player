7c588e3e32a3e2ffadd905a397c11767512bcfe9
kooya3 <elyeestatua@gmail.com>
2023-02-13 00:20:42 +0300

    editing the songcard component to reuse on page

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -1,5 +1,13 @@
-const SongCard = () => (
-  <div>SongCard</div>
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+
+import PlayPause from './PlayPause';
+import { playPause, setActiveSong } from '../redux/features/playerSlice';
+
+const SongCard = ({ song, isPlaying, activeSong, data, i }) => (
+  <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
+  SongCard</div>
 );
 
 export default SongCard;

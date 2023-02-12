3221865dfd979a52b81ed9c780204e18f190bf4e
kooya3 <elyeestatua@gmail.com>
2023-02-12 14:21:17 +0300

    edit on discover section

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,3 +1,19 @@
-const Discover = () => <div>Discover</div>;
+import { Error, Loader, SongCard } from '../components';
+import { genres } from '../assets/constants';
+
+const Discover = () => {
+    console.log(genres)
+
+    return (
+        <div className="flex slex-col">
+            <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
+                <select />
+            </div>
+            <Error />
+            <Loader />
+            <SongCard />
+        </div>
+    );
+};
 
 export default Discover;

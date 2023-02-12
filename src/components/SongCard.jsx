e4924be533161b6a7cca157c109ec8ada5989c68
kooya3 <elyeestatua@gmail.com>
2023-02-13 00:50:05 +0300

    adding titles and subtitles for songcard

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -37,7 +37,7 @@ const SongCard = ({ song, isPlaying, activeSong, data, i }) => {
           <Link to={`/songs/${song?.key}`}>
             {song.title}
           </Link>
-        </p>
+          </p>
         <p className="text-sm truncate text-gray-300 mt-1">
           <Link to={song.artists ? `/artists/${song?.artists[0]?.adamid}` : '/top-artists'}>
             {song.subtitle}

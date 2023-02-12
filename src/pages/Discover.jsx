62c7adfb8c17957ec7f9f593226730755828c7e4
kooya3 <elyeestatua@gmail.com>
2023-02-12 14:33:52 +0300

    updated the UI of the dropdown to change onclick

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -11,17 +11,15 @@ const Discover = () => {
                <h2 className="font-bold text-3xl text-white text-left">
                 Discover
                </h2>
-                <select 
-                onChange={() => {}} 
-                value=""
-                className="bg-black text-gray-300 p-3 rounded-lg text-sm outline-none sm:mt-0 mt-5">
-                {
-                    genres.map((genre) => (
-                        <option>{genre.title}</option>
-                    ))
-                }
-                    </select>
-            </div>
+               <select
+          onChange={(e) => dispatch(selectGenreListId(e.target.value))}
+          value={genreListId || 'pop'}
+          className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
+        >
+          {genres.map((genre) => <option key={genre.value} value={genre.value}>{genre.title}</option>)}
+        </select>
+      </div>
+
         </div>
     );
 };

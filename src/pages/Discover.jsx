8e8e12589615bfa4cdae2cc63d858ce1c698ebe8
kooya3 <elyeestatua@gmail.com>
2023-02-13 02:04:50 +0300

    necessary changes made

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -21,12 +21,12 @@ const Discover = () => {
   return (
     <div className="flex flex-col">
       <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
-        <h2 className="font-bold text-3xl text-white text-left">Discover {genreTitle}</h2>
+        <h2 className="font-bold text-3xl text-yellow-400 text-left">Discover {genreTitle}</h2>
 
         <select
           onChange={(e) => dispatch(selectGenreListId(e.target.value))}
           value={genreListId || 'pop'}
-          className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
+          className="bg-gray-900 text-yellow-300 font-semibold p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
         >
           {genres.map((genre) => <option key={genre.value} value={genre.value}>{genre.title}</option>)}
         </select>

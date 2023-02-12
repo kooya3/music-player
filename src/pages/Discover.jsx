a1beb4b40d58b0c9982efe590eff0a6a142d9b3e
kooya3 <elyeestatua@gmail.com>
2023-02-12 14:29:21 +0300

    changed the drop-down UI

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -2,16 +2,26 @@ import { Error, Loader, SongCard } from '../components';
 import { genres } from '../assets/constants';
 
 const Discover = () => {
-    console.log(genres)
+    const genreTitle = 'pop'
 
     return (
+
         <div className="flex slex-col">
             <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
-                <select />
+               <h2 className="font-bold text-3xl text-white text-left">
+                Discover
+               </h2>
+                <select 
+                onChange={() => {}} 
+                value=""
+                className="bg-black text-gray-300 p-3 rounded-lg text-sm outline-none sm:mt-0 mt-5">
+                {
+                    genres.map((genre) => (
+                        <option>{genre.title}</option>
+                    ))
+                }
+                    </select>
             </div>
-            <Error />
-            <Loader />
-            <SongCard />
         </div>
     );
 };

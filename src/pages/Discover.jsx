a724dcb1cb33682d34d41e583b57d565e99269b6
kooya3 <elyeestatua@gmail.com>
2023-02-12 14:59:14 +0300

    defined a wrapper section on discover page

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -19,6 +19,18 @@ const Discover = () => {
           {genres.map((genre) => <option key={genre.value} value={genre.value}>{genre.title}</option>)}
         </select>
       </div>
+      <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+      {data?.map((song, i) => (
+          <SongCard
+            key={song.key}
+            song={song}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            data={data}
+            i={i}
+          />
+        ))}       
+      </div>
 
         </div>
     );

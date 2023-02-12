efed1b2b68349f834e83969be3988c0e82c74208
kooya3 <elyeestatua@gmail.com>
2023-02-13 01:12:58 +0300

    finished editing play and pause icons

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -1,12 +1,18 @@
+import React from 'react';
 import { FaPauseCircle, FaPlayCircle } from 'react-icons/fa';
 
-
 const PlayPause = ({ isPlaying, activeSong, song, handlePause, handlePlay }) => (isPlaying && activeSong?.title === song.title ? (
-  <FaPauseCircle />
-
-  ) : (
-    <FaPlayCircle />
-    
-);
+  <FaPauseCircle
+    size={35}
+    className="text-green-400"
+    onClick={handlePause}
+  />
+) : (
+  <FaPlayCircle
+    size={35}
+    className="text-gray-300"
+    onClick={handlePlay}
+  />
+));
 
 export default PlayPause;

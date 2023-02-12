f42f3e48ea5abc70d7eb4fa571092f23ecaf092b
kooya3 <elyeestatua@gmail.com>
2023-02-13 01:03:29 +0300

    adding play and pause icons on hover for songcard

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -1,5 +1,12 @@
-const PlayPause = () => (
-  <div>Loader</div>
+import { FaPauseCircle, FaPlayCircle } from 'react-icons/fa';
+
+
+const PlayPause = ({ isPlaying, activeSong, song, handlePause, handlePlay }) => (isPlaying && activeSong?.title === song.title ? (
+  <FaPauseCircle />
+
+  ) : (
+    <FaPlayCircle />
+    
 );
 
 export default PlayPause;

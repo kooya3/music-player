4a4a92acdabe1808fc7364429e78ddebe9e3da05
kooya3 <elyeestatua@gmail.com>
2023-02-12 16:07:13 +0300

    building the endpoints of all the Api's to call

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,16 +1,16 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-  fetch('https://shazam-core.p.rapidapi.com/v1/charts/world', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
-
     export const shazamCoreApi =  createApi({
         reducePath: 'shazamCoreApi',
         baseQuery: fetchBaseQuery({
             baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
-            prepareHeaders: () => {
+            prepareHeaders: (headers) => {
                 headers.set('X-RapidAPI-Key', '1366c740e3msh3cefb15693158ecp19a904jsne1e1d56c2195')
-            }
+            
+                return headers;
+            },
+        }),
+        endpoints: (builder) => ({ 
+            
         })
-    })
+    });

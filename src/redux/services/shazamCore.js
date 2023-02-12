2ebbc5a323726ada5e72980fae49f8d9710d3a31
kooya3 <elyeestatua@gmail.com>
2023-02-12 16:04:58 +0300

    restructured options object in core.js

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,13 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': '1366c740e3msh3cefb15693158ecp19a904jsne1e1d56c2195',
-      'X-RapidAPI-Host': 'shazam-core.p.rapidapi.com'
-    }
-  };
-  
   fetch('https://shazam-core.p.rapidapi.com/v1/charts/world', options)
     .then(response => response.json())
     .then(response => console.log(response))
@@ -15,4 +7,10 @@ const options = {
 
     export const shazamCoreApi =  createApi({
         reducePath: 'shazamCoreApi',
+        baseQuery: fetchBaseQuery({
+            baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+            prepareHeaders: () => {
+                headers.set('X-RapidAPI-Key', '1366c740e3msh3cefb15693158ecp19a904jsne1e1d56c2195')
+            }
+        })
     })
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { configureStore} from '@reduxjs/toolkit';
 
 import playerReducer from './features/playerSlice';
 import { shazamCoreApi } from './services/shazamCore';

ee65865c46b86e9d34d684f8233fe67d6bbc1db9
kooya3 <elyeestatua@gmail.com>
2023-02-13 04:20:54 +0300

    intergrated a carrousel of artists with horizontal scroll

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -58,6 +58,14 @@ const TopPlay = () => {
         </div>
       </div>
 
+      <div className="w-full flex flex-col mt-8">
+        <div className="flex flex-row justify-between items-center">
+          <h2 className="text-white font-bold text-2xl">Top Artists</h2>
+          <Link to="/top-artists">
+            <p className="text-gray-300 text-base cursor-pointer">See more</p>
+          </Link>
+        </div>
+
     </div>
   )
 } 

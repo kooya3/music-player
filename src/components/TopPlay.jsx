f57e5c41723aaf2d23d233ab43e58962dfaf274a
kooya3 <elyeestatua@gmail.com>
2023-02-13 03:58:55 +0300

    defined states and logic to be used to scroll to the top of the page

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -5,15 +5,19 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { FreeMode } from 'swiper';
 
 import PlayPause from './PlayPause';
-
+import { playPause, setActiveSong } from '../redux/features/playerSlice';
+import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 
 import 'swiper/css';
 import 'swiper/css/free-mode';
 
 
 
-const TopPlay = () => (
-  <div>TopPlay</div>
-);
+const TopPlay = () => {
+  const dispatch = useDispatch();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data } = useGetTopChartsQuery();
+  const divRef = useRef(null);
+} 
 
 export default TopPlay;

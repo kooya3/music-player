62f9af3c1c0f4e0c076d3faa892944718c2b5d7e
kooya3 <elyeestatua@gmail.com>
2023-02-13 04:17:12 +0300

    created a op chart and see more section along with styling that it sticks to the right side

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -20,7 +20,7 @@ const TopPlay = () => {
   const divRef = useRef(null);
 
   useEffect(() => {
-    divRef.current.scrollIntoView({ behaviour: 'smooth' }); /* dependency array removed to make sure page goes to top immediately on reload */ });
+    divRef.current.scrollIntoView({ behaviour: 'smooth' }); }); /* dependency array removed to make sure page goes to top immediately on reload */
 
   const topPlays = data?.slice(0, 7);
 
@@ -34,7 +34,14 @@ const TopPlay = () => {
   };
 
   return (
-    <div ref={divRef}>
+    <div ref={divRef} className="xl:ml-6 ml-0 xl:mb-0 mb-6 flex-1 xl:max-w-[500px] max-w-full flex flex-col">
+      <div className="w-full flex flex-col">
+      <div className="flex flex-row justify-between items-center">
+      <h2 className="text-white font-bold text-2xl">Top Charts</h2>
+      <Link to="/top-charts">
+      <p className="text-gray-300 text-base cursor-pointer">See more</p>
+      </Link>
+      </div>
 
     </div>
   )

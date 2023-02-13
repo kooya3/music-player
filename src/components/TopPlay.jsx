ce5f3b588adae310d269269a50189bfb1db75746
kooya3 <elyeestatua@gmail.com>
2023-02-13 03:46:58 +0300

    imported required dependencies for functionality i.e swiper for the horizontal scroll

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -1,3 +1,17 @@
+import React, { useEffect, useRef } from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { FreeMode } from 'swiper';
+
+import PlayPause from './PlayPause';
+
+
+import 'swiper/css';
+import 'swiper/css/free-mode';
+
+
+
 const TopPlay = () => (
   <div>TopPlay</div>
 );

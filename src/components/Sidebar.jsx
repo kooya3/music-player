5a70803740ad72bc4c3439f90f50d7a7160379ad
kooya3 <elyeestatua@gmail.com>
2023-02-13 03:28:22 +0300

    creating an outline of properties for icons on the menu

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -14,8 +14,12 @@ const NavLinks = ({ handleClick }) => (
         to={item.to}
         className="flex flex-row justify-start items-center my-8 text-sm font-medium text-gray-400 hover:text-cyan-400"
         onClick={() => handleClick && handleClick()}
-      >       {item.name}
+      >       
+      <item.icon className="w-6 h-6 mr-2" />
+      {item.name}
       </NavLink>
+
+
     ))}
   </div>
 )
@@ -24,14 +28,24 @@ const NavLinks = ({ handleClick }) => (
 
 const Sidebar = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
   return (
+    <>
     <div className='md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]'>
       <img src={logo} alt="logo" className='
       w-full h-14 object-contain' />
-
       <NavLinks />
+      </div>
 
-    </div>
+     {/*  mobile sidebar */}
+      <div className="absolute md:hidden block top-6 right-3">
+      {!mobileMenuOpen ? (
+          <HiOutlineMenu className="w-6 h-6 mr-2 text-white" onClick={() => setMobileMenuOpen(true)} />
+        ) : (
+          <RiCloseLine className="w-6 h-6 mr-2 text-white" onClick={() => setMobileMenuOpen(false)} />
+        )}
+      </div>
+    </>
   )
 };
 

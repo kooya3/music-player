969d40b19d96c24003003cc695fc3cdbac753e25
kooya3 <elyeestatua@gmail.com>
2023-02-13 03:16:03 +0300

    styling the sidebar of the app

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,6 +6,16 @@ import { RiCloseLine } from 'react-icons/ri';
 import { logo } from '../assets';
 import { links } from '../assets/constants';
 
+const NavLinks = () => (
+  <div className="mt-10">
+    {links.map((item) => (
+      <NavLink className="flex flex-row justify-start items-center my-8 text-sm font-medium text-gray-400 hover:text-cyan-400">
+        {item.name}
+      </NavLink>
+    ))}
+  </div>
+)
+
 
 
 const Sidebar = () => {
@@ -14,6 +24,8 @@ const Sidebar = () => {
     <div className='md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]'>
       <img src={logo} alt="logo" className='
       w-full h-14 object-contain' />
+
+      <NavLinks />
 
     </div>
   )

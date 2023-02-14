0be8644bd4a38c92549f2d04805a9e8b88026b14
kooya3 <elyeestatua@gmail.com>
2023-02-14 09:46:36 +0300

    creating a dynamic component to be used by both artist detail and songdetail

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -1,4 +1,7 @@
-const DetailsHeader = () => (
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const DetailsHeader = ({ artistId, artistData, songData }) => (
   <div>DetailsHeader</div>
 );
 
--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -37,7 +37,6 @@ const SongDetails = () => {
 
 
           </div>
-        </div>
     );
 };
 

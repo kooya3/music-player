2bc5ef28b346ebeaf68da5d3841d8ef053d7957b
kooya3 <elyeestatua@gmail.com>
2023-02-14 09:39:08 +0300

    created logic to display song lyrics

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -12,10 +12,12 @@ const SongDetails = () => {
     const { songid, id: artistId } = useParams();
     const { activeSong, isPlaying } = useSelector((state) => state.player);
 
+    const { data: songData, isFetching: isFetchingSongDetails } = useGetSongDetailsQuery({ songid });
+
     return (
         <div className="flex flex-col">
           <DetailsHeader
-            artistId={artistId}
+            artistId={""}
             songData={songData}
           />
     
@@ -23,6 +25,16 @@ const SongDetails = () => {
           <h2 className="text-white text-3xl font-bold">Lyrics:</h2>
           
           <div className="mt-5">
+          {songData?.sections[1].type === 'LYRICS'
+            ? songData?.sections[1]?.text.map((line, i) => (
+                <p key={`lyrics-${line}-${i}`} className="text-gray-400 text-base my-1">{line}</p>
+                ))
+                : (
+                    <p className="text-gray-400 text-base my-1">Sorry, No lyrics found!</p>
+                    )}
+        </div>
+        </div>
+
 
           </div>
         </div>

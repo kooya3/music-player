a23d5a19cb100142ad5a5da0a4b27c51a1444046
kooya3 <elyeestatua@gmail.com>
2023-02-14 09:52:21 +0300

    creating a dynamic image and setting W and H for Api to return

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -2,7 +2,22 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const DetailsHeader = ({ artistId, artistData, songData }) => (
-  <div>DetailsHeader</div>
+  <div className="relative w-full flex flex-col">
+    <div className="w-full bg-gradient-to-l from-transparent to-black sm:h-48 h-28" />
+
+    <div className="absolute inset-0 flex items-center">
+    <img
+        alt="profile"
+        src={
+          artistId ? artistData?.attributes?.artwork?.url
+            .replace('{w}', '500')
+            .replace('{h}', '500')
+            : songData?.images?.coverart
+          }
+    className="sm:w-48 w-28 sm:h-48 h-28 rounded-full object-cover border-2 shadow-xl shadow-black"
+/>
+
+
 );
 
 export default DetailsHeader;

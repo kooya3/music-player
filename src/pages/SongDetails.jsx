aba2a1fc081c0f7d49fa3a59a59d1812bd0c5b6d
kooya3 <elyeestatua@gmail.com>
2023-02-14 09:34:19 +0300

    added API endpoints to use in songdetails

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
+import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
 
+import { setActiveSong, playPause } from '../redux/features/playerSlice';
 
 
 
 const SongDetails = () => {
+    const dispatch = useDispatch();
     const { songid, id: artistId } = useParams();
+    const { activeSong, isPlaying } = useSelector((state) => state.player);
 
-}
+    return (
+        <div className="flex flex-col">
+          <DetailsHeader
+            artistId={artistId}
+            songData={songData}
+          />
+    
+          <div className="mb-10">
+          <h2 className="text-white text-3xl font-bold">Lyrics:</h2>
+          
+          <div className="mt-5">
+
+          </div>
+        </div>
+    );
+};
 
 export default SongDetails;

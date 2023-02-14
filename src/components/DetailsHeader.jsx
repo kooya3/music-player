b53ed886d2ce18685b61c6a1e0604d3efb65e5cb
kooya3 <elyeestatua@gmail.com>
2023-02-14 10:08:28 +0300

    replaced an earlier file structure with a better one

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const DetailsHeader = ({ artistId, artistData, songData }) => (
+const DetailsHeader = ({ artistId, artistData, songData }) => {
+  const artist = artistData?.artists[artistId]?.attributes;
+
+
+  return(
   <div className="relative w-full flex flex-col">
     <div className="w-full bg-gradient-to-l from-transparent to-black sm:h-48 h-28" />
 
@@ -9,15 +13,31 @@ const DetailsHeader = ({ artistId, artistData, songData }) => (
     <img
         alt="profile"
         src={
-          artistId ? artistData?.attributes?.artwork?.url
-            .replace('{w}', '500')
-            .replace('{h}', '500')
+          artistId ? artist.artwork?.url
+            .replace('{w}', '560')
+            .replace('{h}', '560')
             : songData?.images?.coverart
           }
     className="sm:w-48 w-28 sm:h-48 h-28 rounded-full object-cover border-2 shadow-xl shadow-black"
 />
 
+      <div className="ml-5">
+      <p className="font-bold sm:text-3xl text-xl text-white">
+      {artistId ? artist.name : songData?.title}
+      </p>
+      {!artistId && (
+          <Link to={`/artists/${songData?.artists[0]?.adamid}`}>
+            <p className="text-base text-gray-400 mt-2">{songData?.subtitle}</p>
+            </Link>
+        )}
 
-);
+
+</div>
+    </div>
+
+    <div className="w-full sm:h-44 h-24" />
+  </div>
+      );
+      };
 
 export default DetailsHeader;

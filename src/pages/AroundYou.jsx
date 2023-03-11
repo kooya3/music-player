a1a14893c554033b04c7b4df06a7c5b5864a5d08
kooya3 <elyeestatua@gmail.com>
2023-03-11 21:42:20 +0300

    creating the around you component

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -1,5 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
 
-const CountryTracks = () => <div>CountryTracks</div>;
+import { Error, Loader, SongCard } from '../components';
+import { useGetSongsByCountryQuery } from '../redux/services/shazamCore';
 
-export default CountryTracks;
+const AroundYou = () => {
+    const [country, setCountry] = useState('');
+    const [loading, setLoading] = useState(true);
+    const { activeSong, isPlaying } = useSelector((state) => state.player);
+    const { data, isFetching, error } = useGetSongsByCountryQuery(country);
+   
+    useEffect(() => {
+        axios.get(`https://geo.ipify.org/api/v2/country?apiKey=at_frnuYxGGhoF6IRrNYU2hCq5Pl9Ejw`)
+        .then((res) => setCountry(res?.data?.location.country))
+        .catch((err) => console.log(err))
+        .finally(() => setLoading(false));
+    }, [country]);
+
+
+    if (isFetching && loading) return <Loader title="Loading Songs around you..." />;
+
+    if (error && country !== '') return <Error />;
+  
+
+    return (
+        <div>
+{/* at_frnuYxGGhoF6IRrNYU2hCq5Pl9Ejw */}
+        </div>
+   )
+    }
+
+export default AroundYou;
